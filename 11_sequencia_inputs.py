c622c3aee640835632e047ce713139e5be2631b4
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-09 11:12:28 -0300

    sequencia de inputs

---
+++ 11_sequencia_inputs.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite a sua idade: ')
+pais = input('Digite seu País onde mora: ')
+
+print(f'Olá, meu nome é {nome} eu tenho {idade} e moro no {pais}')
