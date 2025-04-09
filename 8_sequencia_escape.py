1a8935a081ce52bbe30282307ca13877f38810a6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-09 10:56:22 -0300

    variavel de escape

---
+++ 8_sequencia_escape.py
@@ -0,0 +1,12 @@
+# sequencia de escapa
+
+mensagem  = 'Coluna1\tColuna2\tColuna3' # colocando tabulação
+mensagem2 = 'Aprender Python é \nmuito simples.' # colocando sequencia de escape
+mensagem3 = 'c:\\Users\\Paulo Carvalho'
+mensagem4 = 'Aprender "Python" é muito divertido'
+mensagem5 = 'Aprender \'Python\' é muito bom'
+
+print(mensagem)
+print(mensagem2)
+print(mensagem4)
+print(mensagem5)
