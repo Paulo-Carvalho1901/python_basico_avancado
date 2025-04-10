3ac602f72817cfc0eee95925c118f26674fd2a65
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-10 09:30:44 -0300

    convertendo string

---
+++ 12_converter_string.py
@@ -0,0 +1,8 @@
+# O input sempre recebe os dados e returna ele como sting
+# Converter dados
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+print(type(nome))
+print(type(idade))
